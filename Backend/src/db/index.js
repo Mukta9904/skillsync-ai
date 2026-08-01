8b18c412e231dc56b1c43eb4c43a6126c704339f
AnkushMondal <ankushmondal261103@gmail.com>
2026-08-01 21:38:35 +0530

    test

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -6,6 +6,8 @@ const dns = require("dns")
 dns.setServers(["1.1.1.1", "8.8.8.8"])
 
 const connectDB = async () => {
+
+  
   try {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
